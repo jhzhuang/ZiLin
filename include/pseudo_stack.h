36d27e8dc22892e94a9ff64d27c45a9373653502
Zhuang Jing-Heng <skyagathia@outlook.com>
2015-09-15 00:14:10 +0800

    Update pseudo_stack.h

--- include/pseudo_stack.h
+++ include/pseudo_stack.h
@@ -4,7 +4,20 @@
 #include "list.h"
 
 namespace ZiLin {
-
+        
+        typedef char * PStack_Page;
+        
+        List_Head * Get_Page_List_Head(PStack_Page *page) {
+                return (List_Head *)(page);
+        }
+        
+        unsigned int Get_Page_Size(PStack_Page *page) {
+                return *((int *)(page + sizeof(List_Head)));
+        }
+        
+        unsigned int Get_Page_HWM(PStack_Page *page) {
+                return *((int *)(page + sizeof(List_Head)) + 1);
+        }
 }
 
 #endif
