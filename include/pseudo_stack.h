81a4a65da926d384675de24f8a3c569c53b3c4e8
Zhuang Jing-Heng <skyagathia@outlook.com>
2015-09-15 10:55:55 +0800

    Update pseudo_stack.h

--- include/pseudo_stack.h
+++ include/pseudo_stack.h
@@ -5,18 +5,33 @@
 
 namespace ZiLin {
         
-        typedef char * PStack_Page;
+        typedef struct _PStack_Page {
+        	List_Head list_head;
+        	unsigned int size;
+        	unsigned int hwm;
+        	char *page;
+        } PStack_Page;
         
-        List_Head * Get_Page_List_Head(PStack_Page *page) {
-                return (List_Head *)(page);
+        PStack_Page * PStack_Page_Initial(void);
+        
+        void PStack_Page_Delete(PStack_Page *page);
+        
+        inline List_Head * PStack_Page_List_Head(PStack_Page *page) {
+                return &(page->list_head);
         }
         
-        unsigned int Get_Page_Size(PStack_Page *page) {
-                return *((int *)(page + sizeof(List_Head)));
+        inline unsigned int PStack_Page_Size(PStack_Page *page) {
+                return page->size;
         }
         
-        unsigned int Get_Page_HWM(PStack_Page *page) {
-                return *((int *)(page + sizeof(List_Head)) + 1);
+        inline unsigned int PStack_Page_HWM(PStack_Page *page) {
+                return page->hwm;
+        }
+        
+        inline char * PStack_Page_Allocate(PStack_Page *page, unsigned int obj_size) {
+        	unsigned int old_hwm = page->hwm;
+        	page->hwm = old_hwm + obj_size;
+        	return page->page + old_hwm;
         }
 }
 
