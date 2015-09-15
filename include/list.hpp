4e5791a6f9147c880244e8d20d72f9e7e0ec5c4c
Zhuang Jing-Heng <skyagathia@outlook.com>
2015-09-15 09:35:12 +0800

    Create list.hpp

---
+++ include/list.hpp
@@ -0,0 +1,35 @@
+#ifndef _ZILIN_LIST_HPP_
+#define _ZILIN_LIST_HPP_
+
+#include "list.h"
+
+namespace ZiLin {
+
+        template<typename List_Body>
+	class List_Node {
+	public:
+		typedef List_Body Body_Type;
+	public:
+		List_Head head;
+		List_Body body;
+	};
+
+	template<typename Node_Type>
+	class List_Fetch {
+	public:
+		typedef typename Node_Type::Body_Type Body_Type;
+	public:
+		List_Fetch(void) {
+			offset = (unsigned int)(&(((Node_Type *)(0))->body)) - (unsigned int)(&(((Node_Type *)(0))->head));
+		}
+
+		Body_Type *Fetch(List_Head *list_head) {
+			return (Body_Type *)((char *)(list_head) + offset);
+		}
+	private:
+		unsigned int offset;
+	};
+	
+}
+
+#endif
