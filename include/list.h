e14472fce0e9679059c5e551ed99dc28e46365cf
Zhuang Jing-Heng <skyagathia@outlook.com>
2015-09-15 09:33:54 +0800

    split list.h

--- include/list.h
+++ include/list.h
@@ -42,30 +42,6 @@ namespace ZiLin {
 		node->next = 0;
 	}
 
-	template<typename List_Body>
-	class List_Node {
-	public:
-		typedef List_Body Body_Type;
-	public:
-		List_Head head;
-		List_Body body;
-	};
-
-	template<typename Node_Type>
-	class List_Fetch {
-	public:
-		typedef typename Node_Type::Body_Type Body_Type;
-	public:
-		List_Fetch(void) {
-			offset = (unsigned int)(&(((Node_Type *)(0))->body)) - (unsigned int)(&(((Node_Type *)(0))->head));
-		}
-
-		Body_Type *Fetch(List_Head *list_head) {
-			return (Body_Type *)((char *)(list_head) + offset);
-		}
-	private:
-		unsigned int offset;
-	};
 }
 
 #endif
