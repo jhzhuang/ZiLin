97ea3c41e493713039f99d740e87298f9ab1faef
Zhuang Jing-Heng <skyagathia@outlook.com>
2015-09-17 11:33:01 +0800

    Update pseudo_stack.h

--- include/pseudo_stack.h
+++ include/pseudo_stack.h
@@ -5,7 +5,15 @@
 
 namespace ZiLin {
         
+        typedef unsigned int PStack_Id;
         
+        void PStack_List_Initial(void);
+        
+        PStack_Id PStack_List_Add_PStack(void);
+        
+        void PStack_List_Delete_PStack(PStack_Id id);
+        
+        char *PStack_List_Allocate(PStack_Id id, unsigned int obj_size);
 }
 
 #endif
